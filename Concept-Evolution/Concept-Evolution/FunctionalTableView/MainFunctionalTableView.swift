dbef3e978900fe87b755aa8e1cad514163d40cfa
Egon Fiedler <efiedler93@gmail.com>
2018-03-02 16:28:28 -0800

    structure functional

---
+++ Concept-Evolution/Concept-Evolution/FunctionalTableView/MainFunctionalTableView.swift
@@ -0,0 +1,30 @@
+//
+//  MainFunctionalTableView.swift
+//  Concept-Evolution
+//
+//  Created by Egon Fiedler on 3/2/18.
+//  Copyright © 2018 App Solutions. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//Second create a TableCellCallBack that have two parameters, UITableView and IndexPath that is a UITableViewCell
+
+
+//First create the class, it needs to get an item, it should inherit from two classes, NSObject and UITableViewDataSource
+
+    
+    //Third the elements the tableView will hold, an array of those
+
+    
+    //Fourth a variable to hold the structure that will be called back
+
+    
+    //Fifth initialize the the structure with the element that will be called back
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //TableViewElements NumberOfSections, numberOfRowsInSection (how many tableCells there are) and cellForRowAt (safe guarded configure cell, and return that with the parameters of the TableCellCallBack
+
